c6c1717d9ce98c0820cc20a799bd531dbc4da5ea
Nicholas Ricci <naricci@email.neit.edu>
2016-10-25 18:17:54 -0400

    Add window.onload function, switch question values from number/math equations to strings

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -2,26 +2,26 @@
  * Created by nick on 10/25/2016.
  */
 
-var mathProblems = [{question: 35 / 7, answer: 5},
-                {question: 60 / 6, answer: 10},
-                {question: 10 + 10, answer: 20},
-                {question: 3 / 3, answer: 1},
-                {question: 21 / 3, answer: 7},
-                {question: 7 + 2, answer: 9},
-                {question: 5 / 5, answer: 1},
-                {question: 8 - 5, answer: 3},
-                {question: 8 / 8, answer: 1},
-                {question: 18 / 6, answer: 3},
-                {question: 10 - 5, answer: 5},
-                {question: 8 + 1, answer: 9},
-                {question: 4 + 7, answer: 11},
-                {question: 1 * 1, answer: 1},
-                {question: 5 * 7, answer: 35},
-                {question: 9 - 4, answer: 5},
-                {question: 8 - 7, answer: 1},
-                {question: 10 - 5, answer: 5},
-                {question: 8 * 1, answer: 8},
-                {question: 10 + 7, answer: 17}
+var mathProblems = [{question: "35 / 7", answer: 5},
+                {question: "60 / 6", answer: 10},
+                {question: "10 + 10", answer: 20},
+                {question: "3 / 3", answer: 1},
+                {question: "21 / 3", answer: 7},
+                {question: "7 + 2", answer: 9},
+                {question: "5 / 5", answer: 1},
+                {question: "8 - 5", answer: 3},
+                {question: "8 / 8", answer: 1},
+                {question: "18 / 6", answer: 3},
+                {question: "10 - 5", answer: 5},
+                {question: "8 + 1", answer: 9},
+                {question: "4 + 7", answer: 11},
+                {question: "1 * 1", answer: 1},
+                {question: "5 * 7", answer: 35},
+                {question: "9 - 4", answer: 5},
+                {question: "8 - 7", answer: 1},
+                {question: "10 - 5", answer: 5},
+                {question: "8 * 1", answer: 8},
+                {question: "10 + 7", answer: 17}
 ];
 
 var displayProblems = function (problems) {
@@ -38,3 +38,8 @@ var displayProblems = function (problems) {
 
     document.getElementById("mathGrid").innerHTML = str;
 }
+
+window.onload = function () {
+    displayProblems(mathProblems);
+}
+
