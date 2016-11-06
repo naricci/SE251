a63cdad06f194bffc92e89b70bba67310cd43fe8
Nicholas Ricci <naricci@email.neit.edu>
2016-11-06 17:49:41 -0500

    Fix syntax

--- lab4/calendar.js
+++ lab4/calendar.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
     // alert("THIS WORKS");
-    displayCalendar(daysOfWeek);
+    displayCalendar();
 
 
 var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 function displayCalendar(months, years) {
-    var cal += "<table style='border: 2px solid black;'>";
+    var cal;
+    cal += "<table style='border: 2px solid black;'>";
     cal += "<th>";
     for (i = 0; i < daysOfWeek.length; i++)
     {
