e1240f42b59a15e29df0bb16b10850273d3257de
naricci <naricci@email.neit.edu>
2016-12-05 16:27:14 -0500

    Create add button in html, create addActor func to gather user input and store in array

--- lab5/lab5.js
+++ lab5/lab5.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 
-    // Create Add Update Delete Actor Button
-    $('#aud-buttons').append('<input type="button" name="add" value="Add"/>');
-    $('#aud-buttons').append('<input type="button" name="update" value="Update"/>');
-    $('#aud-buttons').append('<input type="button" name="delete" value="Delete"/>');
-
     // Create List of Days for Day Drop-Down
     var d;
     for (d = 1; d < 32; d++) {
@@ -16,6 +11,34 @@ $(document).ready(function () {
     for (y = 1950; y < 2016; y++) {
         $('#year').append('<option value="' + y + '">' + y + '</option>');
     }
+
+    // Array to store submitted actor data in
+    var actors = [];
+
+    // Add Actor on Click Function
+    $('#addActor').on('click', function () {
+        var first = $('#first').val();
+        var last = $('#last').val();
+        var day = $('#day').val();
+        var month = $('#month').val();
+        var year = $('#year').val();
+        var dob = month + "/" + day + "/" + year;
+        var gender = $('input[type=radio]:checked').val();
+        var genre = [];
+        $('input[type=checkbox]:checked').each(function () {
+            genre.push($(this).val());
+        });
+        var actor = {};
+            actor.first = first;
+            actor.last = last;
+            actor.dob = dob;
+            actor.gender = gender;
+            actor.genre = genre;
+            actors.push(actor);
+            
+        $('#actors-list').append('<li><a href="#" class="info" data-actor-id=" + a + ">' + actor.first + " " + actor.last + '</a></li>');
+        alert(JSON.stringify(actor));
+    });
 
 
 });
