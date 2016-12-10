4f9168463755efeb916dcae1a88956838d608a4a
Nicholas Ricci <naricci@email.neit.edu>
2016-12-10 18:35:01 -0500

    Finish up getInfo func w/ delete and update funcs, move doc ready func around

--- lab5/lab5.js
+++ lab5/lab5.js
@@ -1,50 +1,35 @@
-$(document).ready(function () {
+var actors = [];    // Array to store submitted actor data in
+var a = 0;
 
-    // Create List of Days for Day Drop-Down
-    var d;
-    for (d = 1; d < 32; d++) {
-        $('#day').append('<option value="' + d +'">' + d + '</option>');
-    }
+// Add Actor Function
+function addActorInfo() {
 
-    // Create List of Years for Year Drop-Down
-    var y;
-    for (y = 1950; y < 2016; y++) {
-        $('#year').append('<option value="' + y + '">' + y + '</option>');
-    }
-
-    var actors = [];    // Array to store submitted actor data in
-    var a = 0;
-
-    // Add Actor Function
-    $('#addButton').on('click', function () {
-
-        var first = $('#first').val();
-        var last = $('#last').val();
-        var dob = $('#day').val();
-            dob += '/' + $('#month').val();
-            dob += '/' + $('#year').val();
-        var gender = $('input[type=radio]:checked').val();
-        var genre = [];
-        $('input[type=checkbox]:checked').each(function () {
-            genre.push($(this).val());
-        });
-        var actor = {};
-            actor.first = first;
-            actor.last = last;
-            actor.dob = dob;
-            actor.gender = gender;
-            actor.genre = genre;
-            actors.push(actor);
-
-        alert(JSON.stringify(actor));
-        $('#actorsList').append('<li class=" + a + "><a href="#" class="info" data-actor-id=" + a + ">' + actor.first + " " + actor.last + '</a></li>');
-        a++;
-
-        // Remove Update/Delete Buttons
-        $('#updateButton').remove();
-        $('#deleteButton').remove();
+    var first = $('#first').val();
+    var last = $('#last').val();
+    var dob = $('#day').val();
+    dob += '/' + $('#month').val();
+    dob += '/' + $('#year').val();
+    var gender = $('input[type=radio]:checked').val();
+    var genre = [];
+    $('input[type=checkbox]:checked').each(function () {
+        genre.push($(this).val());
     });
+    var actor = {};
+        actor.first = first;
+        actor.last = last;
+        actor.dob = dob;
+        actor.gender = gender;
+        actor.genre = genre;
+        actors.push(actor);
 
+    alert(JSON.stringify(actor));
+    $('#actorsList').append('<li class=" + a + "><a href="#" class="info" data-actor-id=" + a + ">' + actor.first + " " + actor.last + '</a></li>');
+    a++;
+
+    // Remove Update/Delete Buttons
+    $('#updateButton').remove();
+    $('#deleteButton').remove();
+}
     // Get Actor Function
     function getActorInfo() {
 
@@ -68,11 +53,69 @@ $(document).ready(function () {
                 $('input[type=checkbox][value=' + actors[index].genre[i] + ']').prop('checked', true);
             }
             // Create Dynamic Update/Delete Buttons
-            updateButton = '<input type="button" id="updateButton" value="Update Info"/>';
-            deleteButton = '<input type="button" id="deleteButton" value="Delete Actor"/>';
+            updateButton = "<input type='button' id='updateButton' value='Update Info'/>";
+            deleteButton = "<input type='button' id='deleteButton' value='Delete Actor'/>";
 
             $('#addButton').append(updateButton, deleteButton);
+
+            // Delete Actor Info
+            $('#deleteButton').on('click', function () {
+
+                $('.' + index ).remove();
+                $('#updateButton').remove();
+                $('#deleteButton').remove();
+                clearValues();
+                getActorInfo();
+            });
+
+            // Update Actor Info
+            $('#updateButton').on('click', function () {
+
+                $('.'+ index ).remove();
+                var first = $('#first').val();
+                var last = $('#last').val();
+                var dob = $('#day').val();
+                dob += '/' + $('#month').val();
+                dob += '/' + $('#year').val();
+                var gender = $('input[type=radio]:checked').val();
+                var genre = [];
+                $('input[type=checkbox]:checked').each(function () {
+                    genre.push($(this).val());
+                });
+                var actor = {};
+                    actor.first = first;
+                    actor.last = last;
+                    actor.dob = dob;
+                    actor.gender = gender;
+                    actor.genre = genre;
+                    actors.splice(index, 1, actor);
+
+                $('#actorsList').append('<li class=" + a + "><a href="#" class="info" data-actor-id=" + a + ">' + actor.first + " " + actor.last + '</a></li>');
+                $('#updateButton').remove();
+                $('#deleteButton').remove();
+                getActorInfo();
+                clearValues();
+            });
+
         });
     }
-    getActorInfo();
+
+$(document).ready(function () {
+
+    // Create List of Days for Day Drop-Down
+    var d;
+    for (d = 1; d < 32; d++) {
+        $('#day').append('<option value="' + d + '">' + d + '</option>');
+    }
+
+    // Create List of Years for Year Drop-Down
+    var y;
+    for (y = 1950; y < 2016; y++) {
+        $('#year').append('<option value="' + y + '">' + y + '</option>');
+    }
+
+    $('#addButton').on('click', function () {
+        addActorInfo();
+        getActorInfo();
+    });
 });
