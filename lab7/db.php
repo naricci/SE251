3e9bd6550a4a3e6d9254b20b6fea1eebe520a0ee
Nicholas Ricci <naricci@email.neit.edu>
2016-12-12 11:08:25 -0500

    Clean up some changes to overall structure

---
+++ lab7/db.php
@@ -0,0 +1,12 @@
+<?php
+$username = "shop";
+$password = "se266";
+$dsn = "mysql:host=localhost;dbname=actors";
+$db = new PDO($dsn, $username, $password);
+try {
+    $db = new PDO($dsn, $username, $password);
+
+}catch (PDOException $e){
+    exit ("<br />Error: Unable to Connect.");
+}
+?>
---
+++ lab7/updateActor.php
@@ -0,0 +1,26 @@
+<?php
+require('db.php');
+
+$data = json_decode($_POST['data'],true);
+
+$id = $data["id"];
+$firstName = $data["firstName"];
+$lastName = $data["lastName"];
+$DOB = $data["DOB"];
+$gender = $data["gender"];
+$genre = serialize($data["genre"]);
+echo $id;
+
+try {
+    $stmt = $db ->prepare("UPDATE actor SET firstName=:firstName, lastName=:lastName, DOB=:DOB, gender=:gender, genre=:genre WHERE id=:id");
+    $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+    $stmt->bindParam('firstName', $firstName, PDO::PARAM_STR);
+    $stmt->bindParam('lastName', $lastName, PDO::PARAM_STR);
+    $stmt->bindParam('DOB', $DOB, PDO::PARAM_STR);
+    $stmt->bindParam('gender', $gender, PDO::PARAM_STR);
+    $stmt->bindParam('genre', $genre, PDO::PARAM_STR);
+
+    $stmt->execute();
+} catch (PDOException $e) {
+    echo "error";
+}
